7a6619e3e706baf0c999461323f6473a171fa71d
Enrique Lira <enrique.lira97@hotmail.com>
2018-02-14 17:38:17 -0600

    indexing

--- Actividades/indexing.sql
+++ Actividades/indexing.sql
@@ -1,26 +1,20 @@
-create index index1 on orderdetails(orderLineNumber);
-create index index2 on orderdetails(quantityOrdered,orderLineNumber);
-create index index3 on orderdetails(orderLineNumber,quantityOrdered);
-
-select * from orderdetails force index(index1)where orderLineNumber =1 and quantityOrdered >50;
-query_cost=95.20
-select productCode from orderdetails force index(index1)where orderLineNumber =1 and quantityOrdered >50;
-query_cost=95.20
-select orderLineNumber,count(*) from orderdetails force index(index1)where orderLineNumber =1 and quantityOrdered >50 group by orderLineNumber;
-query_cost=95.20
-
-select * from orderdetails force index(index2)where orderLineNumber =1 and quantityOrdered >50;
-query_cost=93.41
-select productCode from orderdetails force index(index2)where orderLineNumber =1 and quantityOrdered >50;
-query_cost=27.62
-select orderLineNumber,count(*) from orderdetails force index(index2)where orderLineNumber =1 and quantityOrdered >50 group by orderLineNumber;
-query_cost=27.62
-
-select * from orderdetails force index(index3)where orderLineNumber =1 and quantityOrdered >50;
-query_cost=8.01
-select productCode from orderdetails force index(index3)where orderLineNumber =1 and quantityOrdered >50;
-query_cost=3.02
-select orderLineNumber,count(*) from orderdetails force index(index3)where orderLineNumber =1 and quantityOrdered >50 group by orderLineNumber;
-query_cost=3.02
-
-/*el costo depende del orden de los valores especificos delado iquierdo  y los rango de lado derecho 
+delimiter //
+create procedure demo_cursor()
+begin
+	declare ids int;
+    declare done int default FALSE;
+    declare cursor1 cursor for select actor_id from actor;
+    declare continue handler
+		for not found
+		set done=true;
+        
+      open cursor1;
+      read_loop:loop
+			  fetch cursor1 into ids;
+			  of done then 
+              leave read_loop;
+			  end if;
+			  select ids;
+			  close cursor1;
+	end
+    delimiter //
